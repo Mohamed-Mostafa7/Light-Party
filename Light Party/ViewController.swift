0a6a5305524a6a6dcd0e52ada4281ea527c90267
Mohamed Mostafa <mohameds.mostafas7@gmail.com>
2024-05-02 15:34:33 +0300

    Initial Commit

---
+++ Light Party/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light Party
+//
+//  Created by Mohamed Mostafa on 02/05/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
